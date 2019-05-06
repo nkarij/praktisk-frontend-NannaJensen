9a1e793827da21c1b361fbbac5073b0038cb8e5d
Nanna <5608562@RTS-365.dk>
2019-05-06 10:51:45 +0200

    Create getAllUrlParams.js

---
+++ 01/dist/getAllUrlParams.js
@@ -0,0 +1,84 @@
+function getAllUrlParams(url) {
+
+    // get query string from url (optional) or window
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+  
+    // we'll store the parameters here
+    var obj = {};
+  
+    // if query string exists
+    if (queryString) {
+  
+      // stuff after # is not part of query string, so get rid of it
+      queryString = queryString.split('#')[0];
+  
+      // split our query string into its component parts
+      var arr = queryString.split('&');
+  
+      for (var i = 0; i < arr.length; i++) {
+        // separate the keys and the values
+        var a = arr[i].split('=');
+  
+        // set parameter name and value (use 'true' if empty)
+        var paramName = a[0];
+        var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+  
+        // (optional) keep case consistent
+        paramName = paramName.toLowerCase();
+        if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+  
+        // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+        if (paramName.match(/\[(\d+)?\]$/)) {
+  
+          // create key if it doesn't exist
+          var key = paramName.replace(/\[(\d+)?\]/, '');
+          if (!obj[key]) obj[key] = [];
+  
+          // if it's an indexed array e.g. colors[2]
+          if (paramName.match(/\[\d+\]$/)) {
+            // get the index value and add the entry at the appropriate position
+            var index = /\[(\d+)\]/.exec(paramName)[1];
+            obj[key][index] = paramValue;
+          } else {
+            // otherwise add the value to the end of the array
+            obj[key].push(paramValue);
+          }
+        } else {
+          // we're dealing with a string
+          if (!obj[paramName]) {
+            // if it doesn't exist, create property
+            obj[paramName] = paramValue;
+          } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+            // if property does exist and it's a string, convert it to an array
+            obj[paramName] = [obj[paramName]];
+            obj[paramName].push(paramValue);
+          } else {
+            // otherwise add the property
+            obj[paramName].push(paramValue);
+          }
+        }
+      }
+    }
+  
+    return obj;
+  }
+
+//   You’ll see how this works soon, but first, here are some usage examples:
+  
+//   getAllUrlParams().product; // 'shirt'
+//   getAllUrlParams().color; // 'blue'
+//   getAllUrlParams().newuser; // true
+//   getAllUrlParams().nonexistent; // undefined
+//   getAllUrlParams('http://test.com/?a=abc').a; // 'abc'
+//   And a demo for you to play around with.
+  
+  
+//   Things to Know Before Using This Function
+//   Our function assumes the parameters are separated by the & character, as indicated in the W3C specifications. However, the URL parameter format in general is not clearly defined, so you occasionally might see ; or &amp; as separators.
+  
+//   Our function still works if a parameter doesn’t have an equals sign or if it has an equals sign but no value.
+  
+//   The values of duplicate parameters get put into an array.
+  
+//   If you just wanted a function you could drop into your code, you’re done now. If you’d like to understand how the function works, read on.
+  
